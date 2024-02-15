e14e4f5c1fd3d1f753b5a57935c5ea9b5efbb25b
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-02-15 17:03:45 +0600

    Create A Reusable Function For Similar Task

--- Parallelogram-area.js
+++ Parallelogram-area.js
@@ -1,13 +1,13 @@
-function calculateParallelogramArea() {
-    // get base of the Parallelogram
-    // const baseInput = document.getElementById('parallelogram-base');
-    // const baseText = baseInput.value;
-    // const base = parseFloat(baseText);
-    // console.log(base);
-    
-    const base = getParallelogramBase();
-    console.log(base);
-}
+// function calculateParallelogramArea() {
+// get base of the Parallelogram
+// const baseInput = document.getElementById('parallelogram-base');
+// const baseText = baseInput.value;
+// const base = parseFloat(baseText);
+// console.log(base);
+
+// const base = getParallelogramBase();
+// console.log(base);
+// }
 
 // function getParallelogramBase(){
 //     const baseInput = document.getElementById("parallelogram-base");
@@ -16,3 +16,28 @@ function calculateParallelogramArea() {
 //     console.log(base);
 //     return base;
 // }
+
+function calculateParallelogramArea() {
+    const base = getInputValueById("parallelogram-base");
+    // console.log('base value', base);
+
+    const height = getInputValueById("parallelogram-height");
+    // console.log('height value', height);
+
+    const area = base * height;
+    console.log("area of the parallelogram is", area);
+    setInnerTextById("parallelogram-area", area);
+}
+
+function getInputValueById(inputFieldId) {
+    const inputField = document.getElementById("parallelogram-base");
+    const inputValueText = inputField.value;
+    const inputValue = parseFloat(inputValueText);
+    // console.log(inputValue);
+    return inputValue;
+}
+
+function setInnerTextById(elementId, area) {
+    const element = document.getElementById(elementId);
+    element.innerText = area;
+}
