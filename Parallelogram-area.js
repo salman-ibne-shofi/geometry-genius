037ecf3b1875afadda3849715881ff4c5664520b
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-02-15 15:12:37 +0600

    Extract Common Code Into A Function

--- Parallelogram-area.js
+++ Parallelogram-area.js
@@ -1,4 +1,18 @@
 function calculateParallelogramArea() {
     // get base of the Parallelogram
-    const baseInput = document.getElementById()
+    // const baseInput = document.getElementById('parallelogram-base');
+    // const baseText = baseInput.value;
+    // const base = parseFloat(baseText);
+    // console.log(base);
+    
+    const base = getParallelogramBase();
+    console.log(base);
 }
+
+// function getParallelogramBase(){
+//     const baseInput = document.getElementById("parallelogram-base");
+//     const baseText = baseInput.value;
+//     const base = parseFloat(baseText);
+//     console.log(base);
+//     return base;
+// }
